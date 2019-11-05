15a2af542f0a8896778ba4658c0f3459164b27e1
jffernandez <jffernandez@fenix-es.com>
2019-11-05 13:53:35 +0100

    Adjust formatting options

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,8 @@ module.exports = {
   semi: false,
   singleQuote: true,
   trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  vueIndentScriptAndStyle: true,
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      For a guide and recipes on how to configure / customize this project,
+      <br />check out the <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
@@ -13,56 +12,63 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
           rel="noopener"
-          >babel</a
         >
+          babel
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
           target="_blank"
           rel="noopener"
-          >typescript</a
         >
+          typescript
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
           target="_blank"
           rel="noopener"
-          >pwa</a
         >
+          pwa
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
           target="_blank"
           rel="noopener"
-          >router</a
         >
+          router
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
           target="_blank"
           rel="noopener"
-          >vuex</a
         >
+          vuex
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
           target="_blank"
           rel="noopener"
-          >eslint</a
         >
+          eslint
+        </a>
       </li>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
           target="_blank"
           rel="noopener"
-          >unit-jest</a
         >
+          unit-jest
+        </a>
       </li>
     </ul>
   </div>

2654f45c7013824807d703a1f2708467b43ef9eb
jffernandez <jffernandez@fenix-es.com>
2019-11-13 16:01:53 +0100

    Test to find config properties

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ module.exports = {
     name: 'nanoAPPs',
     themeColor: '#137bbd',
     backgroundColor: '#00b6ff',
-    theme_color: '#137bbd',
-    background_color: '#00b6ff',
+    theme_color: '#ff7bbd',
+    background_color: '#ffb6ff',
     'apple-mobile-web-app-capable': 'yes', // ESTO NO FUNCIONA!!!!
   },
 }
